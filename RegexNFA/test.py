57491fba8c11f135e8cc15b5837459ac4b4193a3
llgithubll <1468645630@qq.com>
2016-11-27 16:09:30 +0800

    A simple file introduce how to use Regex

---
+++ RegexNFA/test.py
@@ -0,0 +1,26 @@
+from Regex import Re
+
+
+prog = Re.compile('[pj]ython')
+result = prog.match('python')
+if result:
+    print('separate compile and match success')
+
+result = Re.match('[pj]ython', 'jython')
+if result:
+    print('match success')
+
+pattern = Re.compile("d")
+if pattern.search("dog"):     # Match at index 0
+    print('Found it')
+if not pattern.search("dog", 1):   # No match; search doesn't include the "d"
+    print('Not found it')
+
+pattern = Re.compile('[AEIOU]{3}(a|e|i|o|u){3,}')
+assert pattern.match('AEIaei#') is True
+assert pattern.match('AAAaaaa') is True
+
+pattern = Re.compile('^([AEIOUaeiou]|[0123456789]|(@|#)){3,}$')
+assert pattern.match('aaaa') is True
+assert pattern.match('0@#A999') is True
+assert pattern.match('@#') is None
